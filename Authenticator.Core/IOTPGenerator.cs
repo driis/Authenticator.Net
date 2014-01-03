f761e0d3f1a35873227d85fc0afc79bff00434f7
Dennis Riis <dr@driis.dk>
2014-01-03 21:59:57 +0100

    Project skeleton.

---
+++ Authenticator.Core/IOTPGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Authenticator.Core
+{
+    public interface IOTPGenerator
+    {
+        string NextPassword();
+    }
+
+    public class CounterBasedOTPGenerator : IOTPGenerator
+    {
+        public string NextPassword()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
